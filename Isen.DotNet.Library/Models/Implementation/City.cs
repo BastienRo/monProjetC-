b8ae9b164363929625f5925a1ebeb96b11527c5c
MINEMIC-ABAFH3F\brodrigue <bastien.rodrigue@gmail.com>
2018-03-01 20:21:36 +0100

    Test project

---
+++ Isen.DotNet.Library/Models/Implementation/City.cs
@@ -0,0 +1,7 @@
+using System;
+using Isen.DotNet.Library.Models.Base;
+
+namespace Isen.DotNet.Library.Models.Implementation
+{
+    public class City : BaseModel { }
+}
---
+++ Isen.DotNet.Library/Models/Implementation/Person.cs
@@ -0,0 +1,27 @@
+using System;
+using Isen.DotNet.Library.Models.Base;
+
+namespace Isen.DotNet.Library.Models.Implementation
+{
+    public class Person : BaseModel
+    {
+        public string FirstName { get;set; }
+        public string LastName { get;set; }
+        public DateTime? BirthDate { get;set; }
+        public City City { get;set; }
+
+        private string _name;
+        public override string Name 
+        {
+            get { return _name ?? $"{FirstName} {LastName}"; }
+            set { _name = value; }
+        }
+
+        public int? Age => BirthDate.HasValue ?
+            (int?)((DateTime.Now - BirthDate.Value).Days / 365.25) : 
+            null;
+
+        public override string Display =>
+            $"{base.Display}|Age={Age}|City={City}"; 
+    }
+}
